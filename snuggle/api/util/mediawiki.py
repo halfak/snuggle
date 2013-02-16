370b6233443136ab044045afe1e21eab54f63889
aaron.halfaker@gmail.com <aaron.halfaker@gmail.com>
2013-02-16 11:36:49 -0600

    Some work toward user actions

--- snuggle/api/util/mediawiki.py
+++ snuggle/api/util/mediawiki.py
@@ -8,9 +8,9 @@ HTTPError = requests.HTTPError
 class MW:
 	
 	def __init__(self, url):
-		self.url = url
-		self.users = Users(url)
-		self.pages = Pages(url)
+		self.api = MWAPI(url)
+		self.users = Users(self.api)
+		self.pages = Pages(self.api)
 	
 
 class ConnectionError(Exception); pass
@@ -28,76 +28,79 @@ class MWAPI:
 	def __init__(self, url):
 		self.url = url
 	
-	def post(self, params):
+	def post(self, params, cookies=None):
 		params['format'] = "json"
 		
 		try:
 			r = requests.post(
 				self.url,
-				params
+				params,
+				cookies=cookies
 			)
 		except requests.ConnectionError as e:
-			raise MWAPIError("Could not reach the API at %s: %s" % (self.url, e))
+			raise ConnectionError("Could not reach the API at %s: %s" % (self.url, e))
 		except requests.HTTPError as e:
-			raise MWAPIError("The API responded with an error."
+			raise MWAPIError('http', HTTPError)
 		
+		try:
+			doc = json.loads(r.content)
+		except ValueError as e:
+			raise MWAPIError('value', "Could not decode json.")
+		
+		if 'error' in doc:
+			raise MWAPIError(doc['error'].get('code'), doc['error'].get('info'))
+		
+		return doc, r.cookies
 		
 
 class Users:
 	
-	def __init__(self, url):
-		self.url = url
+	def __init__(self, api):
+		self.api = api
 	
 	def authenticate(self, username, password):
-		r = requests.post(
-			self.url,
-			params={
+		doc, cookies  = api.post(
+			{
 				'action': "login",
 				'lgname': username,
 				'lgpassword': password,
 				'format': "json"
 			}
 		)
-		response_doc = json.loads(r.content)
 		
-		r = requests.post(
-			self.url,
-			params={
+		doc, cookies = self.api.post(
+			{
 				'action': "login",
 				'lgname': username,
 				'lgpassword': password,
-				'lgtoken': response_doc['login']['token'],
+				'lgtoken': doc['login']['token'],
 				'format': "json"
 			},
-			cookies = r.cookies
+			cookies = cookies
 		)
 		
-		response_doc = json.loads(r.content)
-		
-		if response_doc['login']['result'] == "WrongPass":
+		if doc['login']['result'] == "WrongPass":
 			raise AuthErrorPass()
-		elif response_doc['login']['result'] == "NotExists":
+		elif doc['login']['result'] == "NotExists":
 			raise AuthErrorName()
-		elif response_doc['login']['result'] == "Success":
+		elif doc['login']['result'] == "Success":
 			return (
-				response_doc['login']['lguserid'], #id 
-				response_doc['login']['lgusername'], #name
-				r.cookies #cookie
+				doc['login']['lguserid'], #id 
+				doc['login']['lgusername'], #name
+				cookies #cookie
 			)
 		else:
-			raise Exception("no")
+			raise Exception("Unexpected login result: %s" % doc['login']['result'])
 		
 	
 class Pages:
 	
-	def init(self, url):
-		self.url = url
+	def init(self, api):
+		self.api = api
 	
 	def append(self, page_name, markup, cookies=None):
-		
-		r = requests.post(
-			self.url,
-			params={
+		doc, cookies = self.api.post(
+			{
 				'action': "query",
 				'prop': "info|revisions",
 				'titles': page_name,
@@ -105,18 +108,31 @@ class Pages:
 			},
 			cookies = cookies
 		)
-		response_doc = json.loads(r.content)
 		
-		token = response_doc['query']
+		try:
+			page = doc['query']['pages'].values()[0]
+		except KeyError as e:
+			raise MWAPIError("API response has unexpected structure.")
+		except IndexError as e:
+			raise MWAPIError("API response has unexpected structure.")
 		
-		r = requests.post(
-			self.url,
-			params={
+		doc, cookies = self.api.post(
+			{
 				'action': "edit",
-				'appendtext': "\n\n" + markup
-				'token': 
+				'title': page_name,
+				'appendtext': "\n\n" + markup,
+				'token': page['edittoken'],
 				'format': "json"
 			},
-			cookies = r.cookies
+			cookies = cookies
 		)
-		response_doc = json.loads(r.content)
+		
+		try:
+			if doc['edit']['result'] == "Success":
+				return 
+		
+		return doc
+		
+	def preview(self, markup, page_name=None):
+		
+		r = 
