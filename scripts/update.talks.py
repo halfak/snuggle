ccef41a60d3b7c819356269d5166677f02bdff05
aaron.halfaker@gmail.com <aaron.halfaker@gmail.com>
2013-07-24 10:20:12 -0500

    Update script tested

--- scripts/update.talks.py
+++ scripts/update.talks.py
@@ -4,7 +4,7 @@ sys.path.append("../")
 
 from snuggle import configuration
 from snuggle import mediawiki
-from snuggle.data import models
+from snuggle.data import models, types
 
 def main():
 	def conf_snuggle(fn):
@@ -24,6 +24,11 @@ def main():
 		type=conf_snuggle,
 		help='the path to Snuggle\'s configuration file'
 	)
+	parser.add_argument(
+		'mediawiki_config',
+		type=conf_mediawiki,
+		help='the math to MediaWiki\'s configuration file'
+	)
 	
 	args = parser.parse_args()
 	
@@ -31,105 +36,115 @@ def main():
 	
 	
 def run(config):
-	model = models.Mongo.from_config(configuration)
+	model = models.Mongo.from_config(config)
 	
 	for user_doc in model.db.users.find({'has_talk_page': True}, fields=['talk', 'name']):
 		
-		print user_doc['name']
+		sys.stderr.write(".")
 		
 		new_threads = []
 		
 		for thread_doc in user_doc['talk']['threads']:
-			modifications = {}
-			if 'level_0' in thread_doc['classes']:
-				modifications['superscript'] = "0"
-			elif 'level_1' in thread_doc['classes']:
-				modifications['superscript'] = "1"
-			elif 'level_2' in thread_doc['classes']:
-				modifications['superscript'] = "2"
-			elif 'level_3' in thread_doc['classes']:
-				modifications['superscript'] = "3"
-			elif 'level_4' in thread_doc['classes']:
-				modifications['superscript'] = "4"
-			
-			new_thread = None
-			if len(thead_doc['classes']) == 0:
-				new_thread = types.Thread(
-					thread_doc['title'],
-					None
-				)
-			elif "welcome" in thread_doc['classes']:
-				new_thread = types.Thread(
-					thread_doc['title'],
-					Trace("welcome message", {})
-				)
+			if 'classes' in thread_doc:
+				modifications = {}
+				if 'level_0' in thread_doc['classes']:
+					modifications['superscript'] = "0"
+				elif 'level_1' in thread_doc['classes']:
+					modifications['superscript'] = "1"
+				elif 'level_2' in thread_doc['classes']:
+					modifications['superscript'] = "2"
+				elif 'level_3' in thread_doc['classes']:
+					modifications['superscript'] = "3"
+				elif 'level_4' in thread_doc['classes']:
+					modifications['superscript'] = "4"
 				
-			elif "deletion" in thread_doc['classes']:
-				new_thread = types.Thread(
-					thread_doc['title'],
-					Trace("deletion notification", {})
-				)
-				
-			elif "vandal" in thread_doc['classes']:
-				new_thread = types.Thread(
-					thread_doc['title'],
-					Trace("vandal warning", modifications)
-				)
-				
-			elif "spam" in thread_doc['classes']:
-				new_thread = types.Thread(
-					thread_doc['title'],
-					Trace("spam warning", modifications)
-				)
-				
-			elif "copyright" in thread_doc['classes']:
-				new_thread = types.Thread(
-					thread_doc['title'],
-					Trace("copyright warning", modifications)
-				)
-				
-			elif "block" in thread_doc['classes']:
-				new_thread = types.Thread(
-					thread_doc['title'],
-					Trace("block", {})
-				)
-				
-			elif "csd" in thread_doc['classes']:
-				new_thread = types.Thread(
-					thread_doc['title'],
-					Trace("csd notification", {})
-				)
-				
-			elif "afc" in thread_doc['classes']:
-				new_thread = types.Thread(
-					thread_doc['title'],
-					Trace("articles for creation", {})
-				)
-				
-			elif "teahouse" in thread_doc['classes']:
-				new_thread = types.Thread(
-					thread_doc['title'],
-					Trace("teahouse invitation", {})
-				)
-				
+				new_thread = None
+				if len(thread_doc['classes']) == 0:
+					new_thread = types.Thread(
+						thread_doc['title'],
+						None
+					)
+				elif "welcome" in thread_doc['classes']:
+					new_thread = types.Thread(
+						thread_doc['title'],
+						types.Trace("welcome message", {})
+					)
+					
+				elif "deletion" in thread_doc['classes']:
+					new_thread = types.Thread(
+						thread_doc['title'],
+						types.Trace("deletion notification", {})
+					)
+					
+				elif "vandal" in thread_doc['classes']:
+					new_thread = types.Thread(
+						thread_doc['title'],
+						types.Trace("vandal warning", modifications)
+					)
+					
+				elif "spam" in thread_doc['classes']:
+					new_thread = types.Thread(
+						thread_doc['title'],
+						types.Trace("spam warning", modifications)
+					)
+					
+				elif "copyright" in thread_doc['classes']:
+					new_thread = types.Thread(
+						thread_doc['title'],
+						types.Trace("copyright warning", modifications)
+					)
+					
+				elif "warning" in thread_doc['classes']:
+					new_thread = types.Thread(
+						thread_doc['title'],
+						types.Trace("general warning", {})
+					)
+					
+				elif "block" in thread_doc['classes']:
+					new_thread = types.Thread(
+						thread_doc['title'],
+						types.Trace("block", {})
+					)
+					
+				elif "csd" in thread_doc['classes']:
+					new_thread = types.Thread(
+						thread_doc['title'],
+						types.Trace("csd notification", {})
+					)
+					
+				elif "afc" in thread_doc['classes']:
+					new_thread = types.Thread(
+						thread_doc['title'],
+						types.Trace("articles for creation", {})
+					)
+					
+				elif "teahouse" in thread_doc['classes']:
+					new_thread = types.Thread(
+						thread_doc['title'],
+						types.Trace("teahouse invitation", {})
+					)
+					
+				else:
+					new_thread = types.Thread(
+						thread_doc['title'],
+						None
+					)
+					print("Encountered an unknown set %(classes)s" % thread_doc)
 			else:
-				print("Encountered an unknown set %(classes)s" % thread_doc)
+				new_thread = types.Thread.deserialize(thread_doc)
 			
-			if new_thread != None:
-				new_threads.append(new_thread)
+			new_threads.append(new_thread.serialize())
 			
-		model.db.users.update(
-			{'_id': user_doc['_id']}, 
-			{'talk.threads': new_threads.serialize()}
-		)
-			
-
-class Trace(serializable.Type):
-	def __init__(self, name, modifications=None):
-		self.name = name
-		self.modifications = modifications if modifications != None else {}
-
-class Thread(serializable.Type):
-	def __init__(self, title, trace=None):
-		self.title = unicode(title)
-		self.trace = Trace.deserialize(trace) if trace != None else None
+		
+		if len(new_threads) > 0:
+			model.db.users.update(
+				{'_id': user_doc['_id']}, 
+				{
+					'$set': {'talk.threads': new_threads}
+				},
+				w=1
+			)
+	
+	sys.stderr.write("\n")
+	
+if __name__ == "__main__": main()
